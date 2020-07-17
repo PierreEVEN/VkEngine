8f9a0cf184c9b12dd07b9ece6ace5c99e4b8b310
Pierre EVEN <evenpierre@orange.fr>
2020-07-17 21:07:11 +0200

    added premake for linux

--- GenerateProjectFiles.sh
+++ GenerateProjectFiles.sh
@@ -4,7 +4,7 @@ if [[ "$OSTYPE" == msys ]]; then
 "Tools/premake5.exe" --file=Sources/Engine/Engine.lua vs2019
 else
   echo "Running premake for CMake"
-"Tools/premake5.exe" --file=Sources/Engine/Engine.lua gmake2
+"Tools/premake5.lin" --file=Sources/Engine/Engine.lua gmake2
 fi
 
 sleep 1
